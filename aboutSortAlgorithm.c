33a288689e83a2a7a62b3e81e2c3f18df4620272
cms-0218 <caomisheng@163.com>
2015-10-29 23:10:23 +0800

    test rm file

--- aboutSortAlgorithm.c
+++ aboutSortAlgorithm.c
@@ -2,9 +2,19 @@
 
 void BubleSort(int *nums,int size)
 {
-	int i;
+	int i,j;
 	for(i = 0; i < size; i++)
 	{
+		for(j = i+1; j < size; j++)
+		{
+			if(nums[j] < nums[i])
+			{
+				int tmp = nums[i];
+				nums[i] = nums[j];
+				nums[j] = tmp;
+			}
+
+		}
 
 	}
 
