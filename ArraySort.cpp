48911b344b0a8b767f33738135022eb96ffd4dc3
cms-0218 <caomisheng@163.com>
2015-10-30 16:15:56 +0800

    test

--- ArraySort.cpp
+++ ArraySort.cpp
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <iostream>
 #include <algorithm>
+#include <cstdlib>
 using namespace std;
 
 void BubbleSort(int *nums,int size)
@@ -45,6 +46,79 @@ void InsertSort(int *nums,int size)
 {
 	if(nums == NULL || size < 2)
 		return;
+	
+	int tmp;
+	for(int i = 0; i < size; i++)
+	{
+		int index = i;
+		tmp = nums[i];
+		for(int j = i-1; j >= 0 ; j--)
+		{
+			if( nums[j] > nums[index])
+			{
+				nums[index] = nums[j];
+				nums[j] = tmp;
+			}
+			index--;
+		}
+	}
+}
+
+
+int main()
+{
+	//============生成测试数据===========
+	const int size = 50;
+	int nums[size];
+	for(int i = 0; i < size; i++)
+		nums[i] = rand();
+	
+	cout << "系统随机生成的测试数据如下：" << endl;
+	for(int i = 0; i < size; i++)
+		cout << nums[i] << "   ";
+	cout << endl;
+	
+	cout << "冒泡排序测试结果如下：" << endl;
+	BubbleSort(nums,size);
+	for(int i = 0; i < size; i++)
+		cout << nums[i] << "   ";
+	cout << "============================================" << endl;
+	
+	cout << "选择排序测试结果如下：" << endl;
+	SelectSort(nums,size);
+	for(int i = 0; i < size; i++)
+		cout << nums[i] << "   ";
+	cout << "============================================" << endl;
+	
+	cout << "插入排序测试结果如下：" << endl;
+	InsertSort(nums,size);
+	for(int i = 0; i < size; i++)
+		cout << nums[i] << "   ";
+	cout << "============================================" << endl;
+	
+	cout << "堆排序测试结果如下：" << endl;
+	for(int i = 0; i < size; i++)
+		cout << nums[i] << "   ";
+	cout << "============================================" << endl;
+	
+	cout << "快速排序测试结果如下：" << endl;
+	for(int i = 0; i < size; i++)
+		cout << nums[i] << "   ";
+	cout << "============================================" << endl;
+	
+	cout << "归并排序测试结果如下：" << endl;
+	for(int i = 0; i < size; i++)
+		cout << nums[i] << "   ";
+	cout << "============================================" << endl;
+	
+	
+	cout << "希尔排序测试结果如下：" << endl;
+	for(int i = 0; i < size; i++)
+		cout << nums[i] << "   ";
+	cout << "============================================" << endl;
+	
+	
+	return 0;
 }
 
 
